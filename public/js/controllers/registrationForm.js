6c8278d381b5ce4c08d200bae5cdf987f0cb7365
Bao Thien Pham <baotpham7@gmail.com>
2017-04-02 10:23:00 -0400

    conntected to google sheet

--- public/js/controllers/registrationForm.js
+++ public/js/controllers/registrationForm.js
@@ -9,8 +9,6 @@ angular.module('app.controllers', [])
 
         function ($scope, $window, RegistersData) {
 
-            var CLIENT_ID = '679590288663-6mae7gt4cgtnoqedcnhbmueeukit7b7u.apps.googleusercontent.com';
-
             //errors handling
             $scope.errorpopup = "";
             $scope.thankyou = false;
@@ -102,7 +100,7 @@ angular.module('app.controllers', [])
                 if(age <= 5){
                     $scope.errorpopup = "";
                     $scope.totalFee = 0;
-                }else if(age >= 6 && age < 11){
+                }else if(age >= 6 && age <= 11){
                     $scope.errorpopup = "";
                     $scope.totalFee = 60;
                 }else if(age >= 12 && age <= 19){
@@ -126,5 +124,64 @@ angular.module('app.controllers', [])
                     console.log("registrationFormCtrl: deleted a member");
                 }
             };
+
+            $scope.submit = function (){
+
+                var address = $scope.mainMember.address + ' ' + $scope.mainMember.address2 + ' ' + $scope.mainMember.city + ' ' +
+                                $scope.mainMember.state + ' ' + $scope.mainMember.zip;
+                $.ajax({
+                    url: "https://docs.google.com/forms/d/e/1FAIpQLSdY2Je4WcNPkMs27aAADbaF4E5iH6biWUhVwZqz18BvbcbMFA/formResponse",
+                    data: {
+                        "entry.852805865"  : $scope.mainMember.firstName,
+                        "entry.1666999625" : $scope.mainMember.lastName,
+                        "entry.1035954706" : $scope.mainMember.shirtSize,
+                        "entry.1509131140" : $scope.mainMember.dob,
+                        "entry.1165623676" : $scope.mainMember.gender,
+                        "entry.736906667" : $scope.mainMember.age,
+                        "entry.906671770" : $scope.mainMember.relationship,
+                        "entry.170736755" : address,
+                        "entry.1725771503" : $scope.mainMember.phone,
+                        "entry.1662824886" : $scope.mainMember.email,
+                        "entry.533389635" : $scope.mainMember.homeChurch,
+                        "entry.1651271441" : $scope.mainMember.churchPhone,
+                        "entry.162274431" : $scope.mainMember.medical,
+                        "entry.372693286" : $scope.mainMember.medicalDesc,
+                        "entry.837390161" : $scope.mainMember.emergencyFirstName,
+                        "entry.57695788" : $scope.mainMember.emergencyLastName,
+                        "entry.706727831" : $scope.mainMember.emergencyPhone,
+                        "entry.1277356253" : $scope.mainMember.emergencyRelationship,
+                        "entry.513632018" : $scope.mainMember.methodOfPayment
+                        // "entry.852805865"  : $scope.mainMember.firstName,
+                        // "entry.1666999625" : "Pham",
+                        // "entry.1035954706" : "S",
+                        // "entry.1509131140" : "01/2",
+                        // "entry.1165623676" : "Male",
+                        // "entry.736906667" : "20",
+                        // "entry.906671770" : "friend",
+                        // "entry.170736755" : "608 Los",
+                        // "entry.1725771503" : "2676251223",
+                        // "entry.1662824886" : "asdf@yahoo.com",
+                        // "entry.533389635" : "my church",
+                        // "entry.1651271441" : "123121111",
+                        // "entry.162274431" : "Yes",
+                        // "entry.372693286" : "i'm sick",
+                        // "entry.837390161" : "emerFisrt",
+                        // "entry.57695788" : "emerLast",
+                        // "entry.706727831" : "2341231111",
+                        // "entry.1277356253" : "father",
+                        // "entry.513632018" : "Cash"
+                    },
+                    type: "POST",
+                    dataType: "xml",
+                    statusCode: {
+                        0: function() {
+                            //Success message
+                        },
+                        200: function() {
+                            //Success Message
+                        }
+                    }
+                });
+            }
 
         }]);
